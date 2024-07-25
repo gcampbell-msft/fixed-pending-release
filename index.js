8dd21593beb65b13de3d49cf5c102ba423c8fc09
Nicholas Wallace <nicholaslwallace@gmail.com>
2024-07-24 19:40:42 -0700

    Move: message creation out of loop

--- index.js
+++ index.js
@@ -8,8 +8,40 @@ async function run() {
     const octokit = github.getOctokit(token);
     const { owner, repo } = github.context.repo;
 
+    // Get workflow inputs
     const label = core.getInput("label", { required: false }) || "fixed-pending-release";
+    const providedMessage = core.getInput("message", { required: false });
+    const isExternalRelease = !!core.getInput("isExternalRelease", { required: false }) || false;
 
+    // Prepare message release message
+    const externalReleaseDefault = ":tada: This issue has now been fixed and is available in the latest release! :tada:";
+    const nonExternalReleaseDefault = ":tada: This issue has now been fixed and is available in [${releaseTag}](${releaseUrl}) :tada:";
+    let message = "";
+
+    // Check if this is a GitHub internal release
+    if (!isExternalRelease) {
+        // if it isn't an external release, we should have access to the release and we can 
+        // template with the most recent release.
+
+        // TODO: Possibly modify to strictly consider official releases.
+        const { data: releases } = await octokit.rest.repos.listReleases({ owner, repo });
+        const release = releases.length > 0 ? releases[0] : undefined;
+
+        if (release === undefined) {
+            throw new Error("There is no release available");
+        }
+
+        message = template(providedMessage || nonExternalReleaseDefault)({
+            releaseName: release.name,
+            releaseTag: release.tag_name,
+            releaseUrl: release.html_url
+        });
+    } else {
+        // in an external release, we simply pass the message
+        message = providedMessage || externalReleaseDefault;
+    }
+
+    // Get list of issues
     const issuesPendingRelease = (await octokit.paginate(octokit.rest.issues.listForRepo, {
         owner,
         repo,
@@ -17,49 +49,15 @@ async function run() {
         per_page: 100
     })).filter(i => i.pull_request === undefined && i.labels.map(l => l.name).includes(label));
     
-    // Get the message template from the user input
-    const externalReleaseDefault = ":tada: This issue has now been fixed and is available in the latest release! :tada:";
-    const nonExternalReleaseDefault = ":tada: This issue has now been fixed and is available in [${releaseTag}](${releaseUrl}) :tada:";
-
     const issuesClosed = [];
     let failedIssues = 0;
 
-    const providedMessage = core.getInput("message", { required: false });
-
+    // Iterate over issues to comment and close them
     for (const issue of issuesPendingRelease) {
         const number = issue.number;
 
         // slow down how often we send requests if there are lots of issues.
         await new Promise((resolve) => setTimeout(resolve, 250));
-        
-        let message = "";
-        const isExternalReleaseInput = core.getInput("isExternalRelease", { required: false });
-        const isExternalRelease = isExternalReleaseInput === "false" ? false : isExternalReleaseInput === "true" ? true : false;
-
-        if (isExternalRelease === undefined) {
-            throw new Error("Invalid input for `isExternalRelease`");
-        }
-
-        // if it isn't an external release, we should have access to the release and we can 
-        // template with the most recent release.
-        if (!isExternalRelease) {
-            // TODO: Possibly modify to strictly consider official releases.
-            const { data: releases } = await octokit.rest.repos.listReleases({ owner, repo });
-            const release = releases.length > 0 ? releases[0] : undefined;
-
-            if (release === undefined) {
-                throw new Error("There is no release available");
-            }
-
-            message = template(providedMessage || nonExternalReleaseDefault)({
-                releaseName: release.name,
-                releaseTag: release.tag_name,
-                releaseUrl: release.html_url
-            });
-        } else {
-            // in an external release, we simply pass the message through.
-            message = providedMessage || externalReleaseDefault;
-        }
 
         try {
             // Comment on the issue that we will close.
